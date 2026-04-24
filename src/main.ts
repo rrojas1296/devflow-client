9ff7ba9a94502bcfba08cfe3cd9411011beec17d
Diego Rojas <rrojas1296@gmail.com>
2026-04-24 17:03:14 -0500

    master: Removed localize

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-/// <reference types="@angular/localize" />
-
-// Supports weights 100-900
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
